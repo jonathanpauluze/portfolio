8b9558c36c9544f477d22e1cd5eb0e7e3e81a923
Jonathan Pauluze <jonathanpauluze@gmail.com>
2022-10-31 22:57:09 -0300

    feat: creating constants file

---
+++ app/constants.ts
@@ -0,0 +1,27 @@
+export const GH_REPOSITORIES_URL =
+  'https://github.com/jonathanpauluze?tab=repositories'
+
+export const REPOSITORIES = [
+  {
+    id: 1,
+    title: 'Portfolio',
+    description: 'My portfolio built with NextJS',
+    gh_url: 'https://github.com/jonathanpauluze/portfolio',
+    deploy_url: 'https://jonathanpauluze.dev/?origin=self',
+  },
+  {
+    id: 2,
+    title: 'Ignite Lab DS',
+    description: 'A Design System created in Ignite Lab event',
+    gh_url: 'https://github.com/jonathanpauluze/ignite-lab-design-system',
+    deploy_url: 'https://jonathanpauluze.github.io/ignite-lab-design-system/',
+  },
+  {
+    id: 3,
+    title: 'Valorant Quiz',
+    description:
+      'A quiz about Valorant developed during the Alura Immersion of React and NextJS.',
+    gh_url: 'https://github.com/jonathanpauluze/valorant-quiz',
+    deploy_url: 'https://jonathanpauluze.github.io/ignite-lab-design-system/',
+  },
+]
