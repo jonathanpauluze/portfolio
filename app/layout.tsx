6652730e3152b37d62b6c02a46490e0747e33afb
Jonathan Pauluze <jonathanpauluze@gmail.com>
2022-10-31 22:56:00 -0300

    fix: adding code image as background on body

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from 'react'
 import { Inter } from '@next/font/google'
-import Image from 'next/image'
 
 import '../styles/globals.css'
 
@@ -17,16 +16,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <title>Jonathan Pauluze - Portfolio</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body>
+      <body className="bg-neutral-900 text-white min-h-screen min-w-screen py-12 px-6 md:px-12 bg-[url('/code.svg')] bg-right-top bg-no-repeat">
         {children}
-
-        <Image
-          className="fixed top-0 right-[-65%] md:right-0 pointer-events-none max-w-[1192px] max-h-[795px] md:max-w-[2385px] md:max-h-[1590px]"
-          src="/code.svg"
-          alt=""
-          width={2385}
-          height={1590}
-        />
       </body>
     </html>
   )
